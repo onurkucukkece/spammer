301d5f42369b8d442333a7d8ab6f0dafa7b14f6f
onur <onurkucukkece@gmail.com>
2017-11-14 23:33:39 +0300

    a bit more interaction

--- spec/spammer_spec.rb
+++ spec/spammer_spec.rb
@@ -9,9 +9,16 @@ describe 'Spammer' do
     answer = cli.ask "Visit where?"
     uri = URI(answer)
     if uri.instance_of?(URI::Generic)
-        uri = URI::HTTP.build({:host => uri.to_s})
+        uri = URI::HTTP.build({ host: uri.to_s })
     end
     visit uri
+
+    loop do
+      action = cli.ask "Click, fill or end"
+      break if action == 'end'
+      what = cli.ask "What?"
+      click_on what if action == 'click'
+    end
     expect_text = cli.ask "Expecting what?"
     expect(page).to have_content(expect_text)
   end
